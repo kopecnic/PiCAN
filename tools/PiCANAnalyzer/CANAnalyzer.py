6702aa1da43dc1ded5ff6ef905e09a0fa1a66bed
kopecnic <kopecnic@msu.edu>
2021-12-05 18:44:18 -0500

    Added styling to output of CANAnalyzer.py

--- tools/PiCANAnalyzer/CANAnalyzer.py
+++ tools/PiCANAnalyzer/CANAnalyzer.py
@@ -1,6 +1,7 @@
 import datetime
 import time
 import can
+from colorama import Fore, Back, Style
 
 ID_DISPLAY_MODES = ['hex', 'dec']
 DATA_DISPLAY_MODES = ['hex', 'dec']
@@ -17,7 +18,7 @@ class Analyzer():
         id_display_mode = 0,
         data_display_mode = 0,
         timestamp_display_mode = 0,
-        print_msg_timeout = 5.0
+        print_msg_timeout = 10.0
         ):
 
         self._busses = busses
@@ -43,12 +44,17 @@ class Analyzer():
     def _on_msg_recieve(self, msg):
 
         dt = 0.0
+        data_changed = True
 
         if (msg.channel + ":" + str(msg.arbitration_id)) in self._msgs_dicts:
             last_recieved_timestamp = self._msgs_dicts[msg.channel + ":" + str(msg.arbitration_id)]['last_recieved_timestamp']
             dt = msg.timestamp - last_recieved_timestamp
+            last_data = self._msgs_dicts[msg.channel + ":" + str(msg.arbitration_id)]['msg'].data
+            if msg.data == last_data:
+                data_changed = False
         
-        self._msgs_dicts[msg.channel + ":" + str(msg.arbitration_id)] = {'last_recieved_timestamp': msg.timestamp, 'msg': msg, 'last_dt': dt}
+        self._msgs_dicts[msg.channel + ":" + str(msg.arbitration_id)] = {'last_recieved_timestamp': msg.timestamp, 'msg': msg, 'last_dt': dt, 'recieved_since_last_print': True,
+            'data_changed': data_changed}
         
         #self.print_msg(msg, dt)
 
@@ -60,19 +66,27 @@ class Analyzer():
 
         print()
 
-        current_time = time.time()
-
         for msg_bus_and_id in self._sorted_msgs(3):
-            if (current_time - self._msgs_dicts[msg_bus_and_id]['last_recieved_timestamp']) < self._print_msg_timeout:
-                msg = self._msgs_dicts[msg_bus_and_id]['msg']
-                dt = self._msgs_dicts[msg_bus_and_id]['last_dt']
-                self.print_msg(msg, dt)
+            self.print_msg(msg_bus_and_id)
         
                 
 
-    def print_msg(self, msg, dt):
+    def print_msg(self, msg_bus_and_id):
+
+        msg = self._msgs_dicts[msg_bus_and_id]['msg']
+        dt = self._msgs_dicts[msg_bus_and_id]['last_dt']
+        current_time = time.time()
+        time_since_last_recieve = current_time - self._msgs_dicts[msg_bus_and_id]['last_recieved_timestamp']
+        recieved_since_last_print = self._msgs_dicts[msg_bus_and_id]['recieved_since_last_print']
+        data_changed = self._msgs_dicts[msg_bus_and_id]['data_changed']
+
+        if time_since_last_recieve > self._print_msg_timeout:
+            return
 
         printString = ''
+
+        if not recieved_since_last_print:
+            printString = printString + Style.DIM
 
         if self._timestamp_display_mode == TIMESTAMP_DISPLAY_MODES[1]:
             printString = printString + 'Dt: {0:6.3f}    '.format(dt) #TODO: need to add dt calculations for each id
@@ -89,16 +103,24 @@ class Analyzer():
         printString = printString + 'Data: '
         if self. _data_display_mode == DATA_DISPLAY_MODES[1]:
             for byte in msg.data:
-                printString = printString + '{0:3d}'.format(byte) + " "
+                if not data_changed:
+                    printString = printString + Style.DIM
+                printString = printString + '{0:3d}'.format(byte) + Style.RESET_ALL + " "
+
             printString = printString + '    '
         else:
             for byte in msg.data:
-                printString = printString + '{0:02X}'.format(byte) + " "
+                if not data_changed:
+                    printString = printString + Style.DIM
+                printString = printString + '{0:02X}'.format(byte) + Style.RESET_ALL + " "
             printString = printString + '    '
+
+        printString = printString + Style.RESET_ALL
 
         print(printString.strip())
 
-    # define our clear function
+        self._msgs_dicts[msg_bus_and_id]['recieved_since_last_print'] = False
+
     def _clear_terminal(self):
     
         print('\033[H\033[J') 
